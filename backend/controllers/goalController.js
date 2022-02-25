dfddf836b6cd51efb9491afb23afb2890b468dd2
Yanivv77 <Yanivv77@gmail.com>
2022-02-26 01:21:51 +0200

    errorMiddleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,35 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc   Get Goals
 //route   Get /api/goals
 //access  Private
-const getGoals = (req, res) => {
-  console.log(req.body)
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals' })
-}
+})
 
 //@desc   Set Goals
 //route   Post /api/goals
 //access  Private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
   res.status(200).json({ message: 'Set goals' })
-}
+})
 
 //@desc   Update Goals
 //route   Put /api/goals/:id
 //access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Update goals' })
-}
+})
 
 //@desc   Delete Goals
 //route   Delete /api/goals/:id
 //access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+})
 
 module.exports = { getGoals, setGoals, updateGoal, deleteGoal }
