7bbcef3b4ba35f2b22d26c66dc0f5008d38cdf40
Yanivv77 <Yanivv77@gmail.com>
2022-02-28 01:56:02 +0200

    Login

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import {useState,useEffect} from 'react'
 import {FaUser}  from 'react-icons/fa'
 
 function Register() {
-  const [fromData , setFromData] = useState ({
+  const [formData , setFormData] = useState ({
     name: "",
     email: "",
     password: "",
@@ -10,12 +10,12 @@ function Register() {
 
   })
 
-  const {name, email,password,password2} = fromData
+  const { name, email, password, password2 } = formData
 
   const onChange = (e) => {
-    setFromData((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
     }))
   }
 
@@ -25,67 +25,67 @@ function Register() {
 
 
   return (
-    
     <>
       <section className='heading'>
         <h1>
-          <FaUser/>Register
+          <FaUser /> Register
         </h1>
         <p>Please create an account</p>
       </section>
 
-      <section className='from'> 
-      <form onSubmit={onSubmit}>
-
-        <div className="form-group">
-        <input type="text" 
-        className='from-control' 
-        id='name' 
-        value={name} 
-        placeholder="Enter your name" 
-        onChange={onChange}/>
-        </div>
-
-        <div className="form-group">
-        <input type="email" 
-        className='from-control' 
-        id='email' 
-        value={email} 
-        placeholder="Enter your email" 
-        onChange={onChange}/>
-        </div>
-
-        <div className="form-group">
-        <input type="password" 
-        className='from-control' 
-        id='password' 
-        value={password} 
-        placeholder="Enter your password" 
-        onChange={onChange}/>
-        </div>
-
-        <div className="form-group">
-        <input type="password" 
-        className='from-control' 
-        id='password2' 
-        value={password2} 
-        placeholder="Confirm password" 
-        onChange={onChange}/>
-        </div>
-
-        <div className="form-group">
-        <button type="submit" 
-        className='btn btn-block' >
-          Submit
-
-        </button>
-        
-        </div>
-
-      </form>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              value={name}
+              placeholder='Enter your name'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              placeholder='Confirm password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
       </section>
-
-
     </>
   )
 }
