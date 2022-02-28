3c7231c1408384636bb251a3f9f2ac0a3fa37c8f
Yanivv77 <Yanivv77@gmail.com>
2022-02-28 22:02:33 +0200

    auth

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducr from '../features/auth/authSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducr,
+  },
 })
