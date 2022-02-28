91d4067c38d3b51bb1edcb7697ca75b880cb0040
Yanivv77 <Yanivv77@gmail.com>
2022-03-01 00:23:23 +0200

    login an register auth

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-
+import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
- 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
 
   return (
     <header className='header'>
@@ -12,13 +20,13 @@ function Header() {
         <Link to='/'>GoalSetter</Link>
       </div>
       <ul>
-        
+        {user ? (
           <li>
-            <button className='btn' >
+            <button className='btn' onClick={onLogout}>
               <FaSignOutAlt /> Logout
             </button>
           </li>
-       
+        ) : (
           <>
             <li>
               <Link to='/login'>
@@ -31,10 +39,10 @@ function Header() {
               </Link>
             </li>
           </>
-        
+        )}
       </ul>
     </header>
   )
-        }
+}
 
 export default Header
