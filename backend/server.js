b69a5dd577097eef2d702731ec7e3c0b71fa7b11
Yanivv77 <Yanivv77@gmail.com>
2022-03-02 01:45:25 +0200

    deploy

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-//const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 connectDB()
 
@@ -31,4 +31,4 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(errorHandler)
 
-//app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
