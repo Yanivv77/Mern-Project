b400041ce22e11794afab49b6c65f9d266c92f85
Yanivv77 <Yanivv77@gmail.com>
2022-03-02 00:34:28 +0200

    dashboard

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducr from '../features/auth/authSlice'
+import authReducer from '../features/auth/authSlice'
+import goalReducer from '../features/goals/goalSlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authReducr,
+    auth: authReducer,
+    goals: goalReducer,
   },
 })
