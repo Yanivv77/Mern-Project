91f9c0d22a537a5a93186fa061c708badddf486a
Yanivv77 <Yanivv77@gmail.com>
2022-03-02 02:46:32 +0200

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
